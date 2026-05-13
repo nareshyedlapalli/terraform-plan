a0cdba8f97e32924c1ee8270f7dcee9d7ae10db2
Naresh <naresh.yedlapalli@fbitn.com>
2026-05-13 08:51:49 -0500

    test: atlantis

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -13,5 +13,5 @@ variable "location" {
 variable "storage_account_name" {
   description = "Name of the storage account"
   type        = string
-  default     = "stgteraformplantest5"  # Must be unique; override via tfvars
+  default     = "stgteraformplantest6"  # Must be unique; override via tfvars
 }
